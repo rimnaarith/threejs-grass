06aad3d1b4d9540189efd7f5f3b542ff951586a9
rimnaarith <rimnaarith@gmail.com>
2025-01-27 15:42:26 +0700

    update renderer

--- src/render/init.ts
+++ src/render/init.ts
@@ -42,7 +42,10 @@ export const initEngine = async () => {
 
   // shadow
   renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputColorSpace = THREE.SRGBColorSpace
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
+  renderer.toneMappingExposure = 1
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
   addOrbitControls()
   const container = document.createElement('div');
